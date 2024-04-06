0cd05c581ccf202c5a5a3fba5c37870a9b55f80f
HaSenugJin <egdg7777@gmail.com>
2024-04-06 12:51:19 +0900

    test

---
+++ ex01.dart
@@ -0,0 +1,7 @@
+import 'data.dart';
+
+void main() {
+  print("hello");
+
+  print(name);
+}
---
+++ ex02.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  int n = 1;
+  double d1 = 10.1;
+  bool b1 = true;
+  String s1 = "홍길동";
+
+  print("정수: ${n}");
+  print("실수: ${d1}");
+  print("부울: ${b1}");
+  print("문자열: ${s1}");
+}
